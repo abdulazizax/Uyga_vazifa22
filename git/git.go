793d4529670812fd154b675564c3bd4ccf04a7c4
Abdulaziz AX <abdulazizax2001@gmail.com>
2024-04-29 13:18:31 +0500

    1-commit main da

---
+++ git/git.go
@@ -0,0 +1,11 @@
+package git
+
+import "os/exec"
+
+func GetUserName() (string, error) {
+	out, err := exec.Command("git", "config", "--global", "user.name").Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
